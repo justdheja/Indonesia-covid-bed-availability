e585de48ab7af473296930147a0148eb611082ee
justdheja <justdheja@gmail.com>
2021-08-01 18:38:27 +0700

    feat: revamp footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,14 +1,21 @@
 const Footer = () => {
 	return (
-		<footer className="text-center bg-gray-100">
-			Thanks to the{' '}
+		<footer className="text-center bg-gray-100 space-x-5">
 			<a
 				href="https://github.com/satyawikananda/rs-bed-covid-indo-api"
 				target="_blank"
 				rel="noopener noreferrer"
-				className="underline"
+				className="underline inline-block"
 			>
 				API
+			</a>
+			<a
+				href="https://github.com/satyawikananda/rs-bed-covid-indo-api"
+				target="_blank"
+				rel="noopener noreferrer"
+				className="underline inline-block"
+			>
+				Repo
 			</a>
 		</footer>
 	);
